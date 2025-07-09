79767676a9bd2d65e4b9a1438a583a7ec014e2ef
Aay-ushi <aayushi.shreya1@gmail.com>
2025-07-10 02:12:55 +0530

    link content print

--- src/test/java/browserStackTest/OpinionSection.java
+++ src/test/java/browserStackTest/OpinionSection.java
@@ -1,7 +1,9 @@
 package browserStackTest;
 
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.openqa.selenium.By;
@@ -18,14 +20,29 @@ public class OpinionSection extends BaseTest{
 		WebDriver driver=getDriver();
 		driver.get(baseURL);
 		Assert.assertTrue(driver.findElement(By.xpath("//*[@id='edition_head' and @data-edition='el-pais']")).isDisplayed());
-		List<WebElement> opinionLinks = driver.findElements(By.cssSelector("a[href*='/opinion/']"));
+		List<WebElement> opinionLinks = driver.findElements(By.xpath("//*[@data-dtm-region='portada_apertura']//a[contains(@href,'https://elpais.com/opinion/')]"));
 		Set<String> articleLinks = new LinkedHashSet<>();
 		for (WebElement link : opinionLinks) {
 				String url = link.getAttribute("href");
-				if (url.contains("/opinion/") && articleLinks.size() < 5) {
+				if(url.isEmpty() || url.equalsIgnoreCase("https://elpais.com/opinion/")){
+					continue;
+				}
+				if (url.contains("/opinion/") && articleLinks.size() < 5 ) {
 						articleLinks.add(url);
+						driver.navigate().to(url);
+						printArticleTitleAndContent(driver);
+						driver.navigate().back();
 				}
 		}
+	}
+
+	public void printArticleTitleAndContent(WebDriver driver){
+		WebElement titleElement=driver.findElement(By.xpath("//*[@id='main-content']//h1"));
+		String title=titleElement.getText();
+		System.out.println(title);
+		WebElement bodyElement=driver.findElement(By.xpath("//*[@data-dtm-region='articulo_cuerpo']"));
+		String body=bodyElement.getText();
+		System.out.println(body);
 	}
 
 }
